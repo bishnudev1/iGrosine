79bca240c54211c2fa9302e32fa7798de564d930
Ankan816 <127026633+Ankan816@users.noreply.github.com>
2024-03-12 01:24:17 +0530

    changes in various files
    Co-authored-by: Bishnudev Khutia <bishnudev1@users.noreply.github.com>

--- client/src/Screens/Login.jsx
+++ client/src/Screens/Login.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import axios from 'axios';
+import Logo from '../Assests/Logo.png';
 
 const Login = () => {
 
 
   return (
-    <>
+    <div className='login-container'>
+      <div className='login-sub-container'>
+        <div>
+        <img src={Logo} alt='logo' height={279} width={400} />
         <h1 className='heading-line'>I-Grocine</h1>
-        <a href='http://localhost:5000/auth/google'>Sign In</a>
-    </>
+        <div className='format'>Login to Your Account</div>
+        <a className='login' href='http://localhost:5000/auth/google'>Sign In with Google</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
