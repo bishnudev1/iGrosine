fa98b175654c41138bb93639019e324340f35d30
Bishnudev Khutia <91979889+bishnudev1@users.noreply.github.com>
2024-06-04 21:35:22 -0700

    feat: bug fixed

--- server/go/routes/item_routes.go
+++ server/go/routes/item_routes.go
@@ -10,7 +10,7 @@ import (
 func ItemRoutes(app *fiber.App) {
 	app.Get("/api/get-items", controllers.GetItems)
 	app.Post("/api/add-item",
-	// middlewars.Auth, 
+	middlewars.Auth, 
 	controllers.AddItem)
 	app.Post("/api/delete-item",
 	
